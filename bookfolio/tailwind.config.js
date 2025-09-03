0a725ba923aeed8ce51fa573b8929f24217cb199
Shaima Rahman <shaimarb02@gmail.com>
2025-09-03 14:35:05 -0400

    Initial commit: Vite + Tailwind portfolio setup

---
+++ bookfolio/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+    content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          paper: { 50: "#f8f6f2", 100: "#f3efe7" },
+        },
+      },
+    },
+    plugins: [],
+  };
+  
